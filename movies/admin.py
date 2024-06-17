a48e17a28d24952192ebde830d68481a94c01d14
MR <mr@MRs-MacBook-Pro.local>
2024-06-17 21:38:32 +0530

    initial commit

---
+++ movies/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import  Genre, Movie
+
+# Register your models here.
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'release_year', 'genre', 'date_created')
+    exclude = ('date_created',)
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
